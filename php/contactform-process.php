0d92414a35607ab354c9db5681be8fc78afb2715
vai1205 <vaibhav.12may95@gmail.com>
2019-12-19 01:45:01 +0530

    processing

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -26,7 +26,7 @@ if (empty($_POST["terms"])) {
 }
 
 $EmailTo = "yourname@domain.com";
-$Subject = "New message from Leno landing page";
+$Subject = "New message from Byeplastik landing page";
 
 // prepare email body text
 $Body = "";
